e529c3cf66e89e704621f3ea4cbb6467295664fe
Hoose <hoose.muu@gmail.com>
2021-03-27 13:00:04 -0600

    ADD: First commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Users from "./Users/index";
+import Todos from "./Todos/index";
+
+import MainLayout from "./Layouts/MainLayout";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <MainLayout>
+        <Route path="/users" render={(props) => <Users {...props} />} />
+        <Route path="/todos" render={(props) => <Todos {...props} />} />
+      </MainLayout>
+    </Router>
   );
 }
 
